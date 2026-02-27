897b4dbb51b46c1a12ed899f23ee7b13e4527941
behappyleee <72903623+behappyleee@users.noreply.github.com>
2026-02-27 12:19:49 +0900

    initial commit

---
+++ src/main/kotlin/be/com/webhook/WebhookApplication.kt
@@ -0,0 +1,11 @@
+package be.com.webhook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebhookApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebhookApplication>(*args)
+}
---
+++ src/test/kotlin/be/com/webhook/WebhookApplicationTests.kt
@@ -0,0 +1,13 @@
+package be.com.webhook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebhookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
